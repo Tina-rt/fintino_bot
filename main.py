b41f9fd3a830eb13f34941a03a82c6f0fac0bef7
toky_RT <rtinahubert@gmail.com>
2023-03-01 22:35:13 +0300

    hub challenge

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.args.get("hub.mode") == "subscribe" and request.args.get('hub.challenge'):
+        if not request.args.get('hub.verify_token') == 'hello':
+            return 'Verification token mismatch', 403
+        print('hello', request.args['hub.challenge'])
+        return request.args['hub.challenge'], 200
+    return "Hello world", 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000),)
