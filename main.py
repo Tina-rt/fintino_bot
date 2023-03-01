94d177f4f3bd593289b0287a435a3558a68c68c1
toky_RT <rtinahubert@gmail.com>
2023-03-01 23:15:32 +0300

    receive message

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ def index():
         return request.args['hub.challenge'], 200
     return "Hello world", 200
 
+@app.route('/', methods=['POST'])
+def webhook():
+    # print(url_for('static', filename='video.mp4'))
+    data = request.get_json()
+    log(data)
+    # typing_off()
+    for entry in data['entry']:
+        for messaging_event in entry['messaging']:
+            
+            sender_id = messaging_event['sender']['id']
+            
+            # typing_on(sender_id)
+            if 'message' in messaging_event:
+                if 'text' in messaging_event['message'] and 'quick_reply' not in messaging_event['message']:
+                    query = messaging_event['message']['text']
+                    print(query)
+    return "ok", 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000),)
